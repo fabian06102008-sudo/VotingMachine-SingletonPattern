c5f9e74d2ea4d62eae9bbecb7be40c88e7300ec0
Fabian <Fabian@FABIANS_LAPTOP>
2026-01-20 09:53:14 +0100

    Projektdateien hinzufügen.

---
+++ SingleTonPattern/VotingPeople.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTonPattern
+{
+    internal class VotingPeople
+    {
+        private string FirstName {  get; set; }
+        private string LastName { get; set; }
+        private int Age { get; set; }
+        private string FingerPrint { get; set; }
+
+        public VotingPeople(string firstname, string lastname, int age, string fingerprint)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Age = age;
+            FingerPrint = fingerprint;
+        }
+
+        public override string ToString()
+        {
+            return $"| Firstname: {FirstName}, Lastname: {LastName}, Age: {Age}, Private Fingerprint: {FingerPrint} |";
+        }
+
+    }
+}
